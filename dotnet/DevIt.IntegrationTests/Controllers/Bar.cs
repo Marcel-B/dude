f1c10224d7b394bcdc9c3013b12079e247f6ebcf
Marcel Benders <marcel.benders@netto-online.de>
2023-01-12 09:21:52 +0100

    Testklasse

---
+++ dotnet/DevIt.IntegrationTests/Controllers/Bar.cs
@@ -0,0 +1,43 @@
+﻿using System.Net;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Xunit;
+
+namespace DevIt.IntegrationTests
+{
+  [Collection("ApiTestFixtureCollection")]
+  public class AbrechnungControllerTests : IClassFixture<IntegrationTestsFixture>
+  {
+    private const string UserId = "Norbert Kahl";
+    private readonly HttpClient _client;
+    private readonly IServiceProvider _serviceProvider;
+
+    public AbrechnungControllerTests(IntegrationTestsFixture apiTestsFixture)
+    {
+      // _serviceProvider = apiTestsFixture.Services;
+      // _client = apiTestsFixture
+      //     .ConfigureAuth(UserId)
+      //     .CreateClient(new WebApplicationFactoryClientOptions
+      //     {
+      //         BaseAddress = new Uri("http://localhost/api/"),
+      //         AllowAutoRedirect = false
+      //     });
+    }
+
+    [Fact]
+    public async Task TutDinge()
+    {
+      // Arrange
+      // var id = "1234";
+      // var expected = File.ReadAllText("./Controllers/__snapshots__/foo.json");
+      //
+      // // Act
+      // var response = await _client.GetAsync($"abrechnung/by-month");
+      //
+      // // Assert
+      // response.StatusCode.Should().Be(HttpStatusCode.OK);
+      // var jsonString = await response.Content.ReadAsStringAsync();
+      // jsonString.Should().BeEquivalentTo(expected);
+    }
+  }
+}
