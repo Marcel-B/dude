b20438a90506594028f6c9d94cc2d71a6296cae7
Marcel Benders <marcel.benders@outlook.de>
2023-01-15 00:41:28 +0100

    ssl

--- dotnet/Baldors.Gate/Program.cs
+++ dotnet/Baldors.Gate/Program.cs
@@ -4,7 +4,7 @@ var services = builder.Services;
 services
   .AddReverseProxy()
   .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
-
+services.AddLettuceEncrypt();
 var app = builder.Build();
 app.UseRouting();
 app.UseEndpoints(endpoints => { endpoints.MapReverseProxy(); });
