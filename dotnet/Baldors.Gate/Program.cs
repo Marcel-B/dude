72c9a9c7535f469af73b1aea1c7ed4fe5bbf9fbe
Marcel Benders <marcel.benders@outlook.de>
2023-01-15 00:53:45 +0100

    proxy ssl

--- dotnet/Baldors.Gate/Program.cs
+++ dotnet/Baldors.Gate/Program.cs
@@ -1,10 +1,16 @@
+using LettuceEncrypt;
+
 var builder = WebApplication.CreateBuilder(args);
 var services = builder.Services;
 
 services
   .AddReverseProxy()
   .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
-services.AddLettuceEncrypt();
+
+services
+  .AddLettuceEncrypt()
+  .PersistDataToDirectory(new DirectoryInfo("/app/config"), "Password123");
+
 var app = builder.Build();
 app.UseRouting();
 app.UseEndpoints(endpoints => { endpoints.MapReverseProxy(); });
